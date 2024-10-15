c68b69ac3c4d846776398b50cd11334462cb5fa4
Alejandro Martínez <alejandromartinezescobedo@gmail.com>
2024-10-15 12:27:25 -0600

    Actualización del repositorio

--- AppCalcDemo/ViewController.m
+++ AppCalcDemo/ViewController.m
@@ -13,9 +13,23 @@
     
 }
 @property (weak, nonatomic) IBOutlet UITextField *texto1;
+@property (weak, nonatomic) IBOutlet UITextField *texto2;
+@property (weak, nonatomic) IBOutlet UITextField *texto3;
+
+
 @property (weak, nonatomic) IBOutlet UIButton *botonIn;
 @property (weak, nonatomic) IBOutlet UIButton *botonDn;
+@property (weak, nonatomic) IBOutlet UIButton *botonInM;
+@property (weak, nonatomic) IBOutlet UIButton *botonDnM;
+@property (weak, nonatomic) IBOutlet UITextField *botonInA;
+@property (weak, nonatomic) IBOutlet UIButton *botonDnA;
+
 @property (strong, nonatomic) NSNumberFormatter *numberFormatter;
+@property (strong,nonatomic) NSArray *operaciones;
+@property (weak, nonatomic) IBOutlet UIPickerView *pickerOperaciones;
+
+@property (weak, nonatomic) IBOutlet UIPickerView *pickerOP;
+
 @end
 
 @implementation ViewController
@@ -28,6 +42,34 @@
     self.texto1.text = @"0";
     self.numberFormatter = [[NSNumberFormatter alloc]init];
     self.numberFormatter.numberStyle = NSNumberFormatterDecimalStyle;
+    
+    //PICKERVIEW
+    NSArray *operaciones = @[@"+",@"-", @"*", @"/"];
+    
+    NSArray *meses = @[@"Enero", @"Febrero", @"Marzo", @"Abril", @"Mayo", @"Junio", @"Julio", @"Agosto", @"Septiembre", @"Octubre", @"Noviembre", @"Diciembre"];
+
+}
+//PickerView
+-(NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView{
+    return 1;
+}
+-(NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component{
+    
+    return self.operaciones.count;
+}
+
+- (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component
+ {
+    return self.operaciones[row];
+
+}
+
+//Acciones al seleccionar una operación
+
+- (void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component {
+    NSString *operacionSeleccionada = self.operaciones[row];
+    // Aquí puedes realizar alguna acción con la operación seleccionada, como mostrar un mensaje o realizar un cálculo
+    NSLog(@"Operación seleccionada: %@", operacionSeleccionada);
 }
 
 - (IBAction)botonDn:(UIButton *)sender {
@@ -41,6 +83,29 @@
     valorActual++;
     self.texto1.text = [NSString stringWithFormat:@"%d",valorActual];
 }
+- (IBAction)botonInM:(UIButton *)sender {
+    int valorActual = [self.texto2.text intValue];
+    valorActual++;
+    self.texto2.text = [NSString stringWithFormat:@"%d", valorActual];
+}
+
+- (IBAction)botonDnM:(UIButton *)sender {
+    int valorActual = [self.texto2.text intValue];
+    valorActual--;
+    self.texto2.text = [NSString stringWithFormat:@"%d", valorActual];
+}
+
+- (IBAction)botonInA:(UIButton *)sender {
+    int valorActual = [self.texto3.text intValue];
+    valorActual++;
+    self.texto3.text = [NSString stringWithFormat:@"%d",valorActual];
+}
+- (IBAction)botonDnA:(UIButton *)sender {
+    int valorActual = [self.texto3.text intValue];
+    valorActual--;
+    self.texto3.text = [NSString stringWithFormat:@"%d", valorActual];
+}
+
 
 -(BOOL)_texto1:(UITextField *)texto1 shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string{
     NSString *resultString =[texto1.text stringByReplacingCharactersInRange:range  withString:string];
@@ -50,4 +115,6 @@
     NSPredicate *predicate = [NSPredicate predicateWithFormat:@"Solo hace match con %@", regexString];
     return [predicate evaluateWithObject:resultString];
 }
+
+
 @end
